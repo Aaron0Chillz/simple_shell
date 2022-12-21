4197a1142f252349d81faaf8a6c52204f3fc9b69
Aaron0Chillz <charlesunlimited10@gmail.com>
2022-12-21 10:29:09 -0800

    builtin module

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+/**
+ * main - entry point
+ * @ac: arg count 
+ * @av: arg vector 
+ * @env: environmental variables
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int ac, char **av, char **env)
+{
+	(void)ac;
+	(void)av;
+
+
+	return(0);
+}
